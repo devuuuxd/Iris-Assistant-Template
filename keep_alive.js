9fac489fe36a27d47c8fae22f9e8d63bae7f1fc5
Devansh OP <91201468+DevanshxDop@users.noreply.github.com>
2024-02-22 09:15:43 +0530

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -1,6 +1,6 @@
-var http = require("http")
+var http = require('http');
 
-http.createServer(function(req,res
-                          res.write("I'm Alive")
-                          res.end()
-                          )).listen(8080);
+http.createServer(function (req, res) {
+  res.write("I'm alive");
+  res.end();
+}).listen(8080);
